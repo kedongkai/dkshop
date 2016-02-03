ea2f19e61473a20ba4b1805464ca03548d077ee6
Dongkai Ke <kedongkai@gmail.com>
2016-02-03 18:20:03 +1100

    Initial commit.

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+	public class NinjectDependencyResolver : IDependencyResolver
+	{
+		private IKernel kernel;
+
+		public NinjectDependencyResolver(IKernel kernelParam)
+		{
+			kernel = kernelParam;
+			AddBindings();
+		}
+
+		public object GetService(Type serviceType)
+		{
+			return kernel.TryGet(serviceType);
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return kernel.GetAll(serviceType);
+		}
+
+		private void AddBindings()
+		{
+			/*
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new List<Product> {
+				new Product { Name = "Football", Price = 25 },
+				new Product { Name = "Surf board", Price = 179 },
+				new Product { Name = "Running shoes", Price = 95 }
+			});
+			kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+			*/
+			kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+			Mock<IOrderProcessor> mock = new Mock<IOrderProcessor>();
+			kernel.Bind<IOrderProcessor>().ToConstant(mock.Object);
+			/*
+			EmailSettings emailSettings = new EmailSettings
+			{
+				WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+			};
+			kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
+			*/
+		}
+	}
+}
